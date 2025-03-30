927b9c3b9ec0c3068be09b5de088ceefe71d5ffe
dhruvathejas <dhruvathejasb@gmail.com>
2025-03-30 18:01:48 +0530

    Center the square

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+let side_length = 80
+turtle.setPositionCartesian(0 - side_length / 2, side_length / 2)
 for (let index = 0; index < 4; index++) {
-    turtle.moveTurtleDirection(TurtleDirection.Forward, 0)
+    turtle.moveTurtleDirection(TurtleDirection.Forward, side_length)
     turtle.turnTurtleDirectionByDegrees(TurtleTurnDirection.Right, 90)
 }
